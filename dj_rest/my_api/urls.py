0dd04dbc9c9e6baf735c768d0b596884ed13a77a
hendra <hendradev8@gmail.com>
2021-02-15 16:53:56 +0700

    initial commit

---
+++ dj_rest/my_api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from .views import article_list, article_detail, article_b_list, article_b_detail, ArticleAPIView, ArticleDetails, \
+    ArticleGAList, ArticleGADetail, ArticleViewSet, ArticleGenViewSet, ArticleMVSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
+router.register('gvs-article', ArticleGenViewSet, basename='gvs-article')
+router.register('mvs-article', ArticleMVSet, basename='mvs-article')
+
+urlpatterns = [
+    path('article/', article_list),
+    path('detail/<int:pk>/', article_detail),
+    path('article-b/', article_b_list),
+    path('detail-b/<int:pk>/', article_b_detail),
+    path('article-c/', ArticleAPIView.as_view()),
+    path('detail-c/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/', ArticleGAList.as_view()),
+    path('generic/article/<int:id>/', ArticleGADetail.as_view()),
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
+]
